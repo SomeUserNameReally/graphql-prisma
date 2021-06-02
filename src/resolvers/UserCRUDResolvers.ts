845c8678b0da016f2e7472c52a3dc0eb6d9e5588
Ajay Singh <ajayhasanemail@gmail.com>
2021-06-02 01:34:04 -0400

    Make default calculated skip value 0 in users resolver.

--- src/resolvers/UserCRUDResolvers.ts
+++ src/resolvers/UserCRUDResolvers.ts
@@ -44,7 +44,7 @@ export class UserCRUDResolvers {
         // Default values can be kept in sync with
         // global variables or db data.
         const take = _take >= 0 && _take <= 100 ? _take : 10;
-        const skip = _skip >= 0 ? _skip : 10;
+        const skip = _skip >= 0 ? _skip : 0;
 
         const base: Partial<FindManyUserArgs> = {
             take,
