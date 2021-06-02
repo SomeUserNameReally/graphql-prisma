d017bae68bcbd594a3eb44814a56cb4ff22ec770
Ajay Singh <ajayhasanemail@gmail.com>
2021-06-02 02:10:23 -0400

    Add take and skip parameters for cuistom FindManyPostArgs args.

--- src/types/args/PostCRUDArgs.ts
+++ src/types/args/PostCRUDArgs.ts
@@ -1,9 +1,20 @@
-import { ArgsType, Field } from "type-graphql";
+import { ArgsType, Field, Int } from "type-graphql";
+import { BaseCRUDArgs } from "./BaseCRUDArgs";
 
 @ArgsType()
-export class FindManyPostArgs {
+export class FindManyPostArgs implements BaseCRUDArgs {
     @Field((_type) => String, {
         nullable: true
     })
     query?: string;
+
+    @Field((_type) => Int, {
+        nullable: true
+    })
+    take: number = 10;
+
+    @Field((_type) => Int, {
+        nullable: true
+    })
+    skip: number = 0;
 }
