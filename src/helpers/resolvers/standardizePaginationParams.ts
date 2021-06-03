0f0ce34139037e7ecc33d07ede2ba4800991a7ba
Ajay Singh <ajayhasanemail@gmail.com>
2021-06-02 22:19:30 -0400

    Make standardizePaginationParams helper return orderBy in addition and make related changes.

--- src/helpers/resolvers/standardizePaginationParams.ts
+++ src/helpers/resolvers/standardizePaginationParams.ts
@@ -1,16 +1,36 @@
 import { BaseCRUDArgs } from "../../types/args/BaseCRUDArgs";
+import { GenericObject } from "../../typings/utils";
 
-export const standardizePaginationParams = <T extends BaseCRUDArgs>({
+interface StandardizePaginationParams<T> {
+    take: number;
+    skip: number;
+    orderBy?: [T] | undefined;
+}
+
+// TODO: I'm sure there's a TypeGraphQL/Prisma way of
+// omitting certain fields.
+
+// It may not apply to every scenario though...
+export const standardizePaginationParams = <
+    U extends GenericObject = GenericObject & {
+        password?: "asc" | "desc" | undefined;
+    },
+    T extends BaseCRUDArgs<U> = BaseCRUDArgs<U>
+>({
     take: _take,
-    skip: _skip
-}: T) => {
+    skip: _skip,
+    orderBy: _orderBy
+}: T): StandardizePaginationParams<U> => {
     // Default values can be kept in sync with
     // global variables or db data.
     const take = _take >= 0 && _take <= 100 ? _take : 10;
     const skip = _skip >= 0 ? _skip : 0;
+    const pagination: StandardizePaginationParams<U> = { take, skip };
 
-    return {
-        take,
-        skip
-    };
+    if (_orderBy) {
+        delete _orderBy.password;
+        if (Object.keys(_orderBy).length !== 0) pagination.orderBy = [_orderBy];
+    }
+
+    return pagination;
 };
