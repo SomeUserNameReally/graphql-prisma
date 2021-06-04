5cf25fd1b00ccd0489e43ff16cd360a16f3f8fb7
Ajay Singh <ajayhasanemail@gmail.com>
2021-06-03 21:38:37 -0400

    Refine global setup script for jest to use newly defined isRunning method.

--- __test__/jest/setup.ts
+++ __test__/jest/setup.ts
@@ -1,6 +1,8 @@
 const { Server } = require("../../src/Server");
 
 module.exports = async () => {
+    if (Server.isRunning()) return;
+
     Object.defineProperty(global, "__closeServer__", {
         value: Server.close
     });
