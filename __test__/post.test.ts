c3efe08627bb76c395cfdc0b0ee8826116bcd0e3
Ajay Singh <ajayhasanemail@gmail.com>
2021-06-04 18:32:52 -0400

    Add test "Should expose own private posts on authentication."

--- __test__/post.test.ts
+++ __test__/post.test.ts
@@ -2,7 +2,7 @@ import { gql } from "apollo-boost";
 import { PrismaClient } from "../src/prisma/client";
 import { Post } from "../src/prisma/generated/type-graphql";
 import { getApolloClient } from "./helpers/getApolloClient";
-import { seedDatabase } from "./helpers/seedDatabase";
+import { seedDatabase, userInfo } from "./helpers/seedDatabase";
 
 const client = getApolloClient();
 const prisma = PrismaClient.client;
@@ -51,5 +51,37 @@ describe("Test Post model related functionality", () => {
         // }});
 
         // expect(allPublishedPosts).toBe(posts.length);
+    });
+
+    test("Should expose own private posts on authentication", async () => {
+        const authClient = getApolloClient(userInfo.jwt);
+        const QUERY_NAME = "posts";
+
+        const {
+            data: { posts }
+        } = await authClient.query<{
+            [QUERY_NAME]: Partial<Post>[];
+        }>({
+            query: gql`
+                query {
+                    ${QUERY_NAME}(own: true) {
+                        published
+                        author {
+                            id
+                        }
+                    }
+                }
+            `
+        });
+        expect(Array.isArray(posts)).toBe(true);
+        expect(posts.length).toBeGreaterThan(0);
+        expect(typeof userInfo.data?.id).toBe("string");
+        posts.forEach((post) =>
+            expect(post.author && post.author.id).toBe(userInfo.data!.id)
+        );
+        // Not necessarily true, but
+        // we made the data up so...
+        // Also, `!post.published !== post.published === false`.
+        expect(posts.some((post) => post.published === false)).toBe(true);
     });
 });
