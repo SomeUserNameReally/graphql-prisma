a838998949286a40e592b444ca2239753e36b815
Ajay Singh <ajayhasanemail@gmail.com>
2021-06-03 21:41:15 -0400

    Augment beforeAll hook to create dummy data and additionaly remove posts data for all tests.

--- __test__/user.test.ts
+++ __test__/user.test.ts
@@ -1,12 +1,52 @@
 import ApolloBoost, { gql } from "apollo-boost";
 import { PrismaClient } from "../src/prisma/client";
 import fetch from "node-fetch";
+import { hashSync as bcryptHashSync } from "bcrypt";
 
 const client = new ApolloBoost({ uri: "http://localhost:4000", fetch });
 
 describe("Test User model related functionality", () => {
     beforeAll(async () => {
+        await PrismaClient.client.post.deleteMany();
         await PrismaClient.client.user.deleteMany();
+
+        const user = await PrismaClient.client.user.create({
+            data: {
+                email: "default@example.com",
+                firstName: "Default",
+                lastName: "User",
+                password: bcryptHashSync(
+                    "abc1234",
+                    +(process.env.BCRYPT_SALT_ROUNDS || 10)
+                ),
+                age: 30
+            }
+        });
+
+        await PrismaClient.client.post.create({
+            data: {
+                title: "Post one",
+                body: "This is post one.",
+                author: {
+                    connect: {
+                        id: user.id
+                    }
+                }
+            }
+        });
+
+        await PrismaClient.client.post.create({
+            data: {
+                title: "Post two",
+                body: "This is post two.",
+                author: {
+                    connect: {
+                        id: user.id
+                    }
+                },
+                published: false
+            }
+        });
     });
 
     afterAll(async () => {
